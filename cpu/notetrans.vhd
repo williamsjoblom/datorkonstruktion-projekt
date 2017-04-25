5c8b392b8dea66b3c5dadb773d9a918f63328d0e
Alexander Zeijlon <alexander.zeijlon@gmail.com>
2017-04-25 15:45:00 +0200

    some progress

--- cpu/notetrans.vhd
+++ cpu/notetrans.vhd
@@ -8,22 +8,39 @@ entity notetrans is
   port(clk: in std_logic;
        ch0: in std_logic;
        ch1: in std_logic;
-       wr: in std_logic;
-       note_data: in unsigned(7 downto 0)
+       rdy: in std_logic;
+       wr_enable: in std_logic;
+       in_data: in unsigned(7 downto 0);
+       out_data: out unsigned(7 downto 0)
        )
 end notetrans;
 
 architecture Behavioral of notetrans is
 
   -- Internal signals
+  signal notep : std_logic_vector(3 downto 0);
+  signal octavep : std_logic_vector(3 downto 0);
+  signal ch0_int : std_logic := 0;
+  signal ch1_int : std_logic := 0;
+  signal FC : std_logic := 0;           -- Fine/Course
+
+
+  notep <= in_data(7 downto 4);
+  octavep <= in_data(3 downto 0);
   
-begin
-  
-  process(clk)
-  begin
-    if rising_edge(clk) then
-      
-    end if;
-  end process;
+  -- Channel Register.
+process(clk) begin
+   if rising_edge(clk) then
+     if rst = '1' then
+       ch0_int <= '0';
+       ch1_int <= '0';
+     else
+       if wr_enable = '1' then
+         ch0_int <= ch0;
+         ch1_int <= ch1;
+       end if;
+     end if;
+   end if;
+end process;
 
 end Behavioral;
